c4253ee569fba308c7f334279465875113e4c20c
fotopoulos-v <fotopoulos.v2@gmail.com>
2025-12-07 20:43:03 +0200

    changes vol29 to align the filters

--- BoardGame-Scout.py
+++ BoardGame-Scout.py
@@ -67,8 +67,8 @@ st.markdown("""
     }
     [data-testid="stExpander"] .stMultiSelect [data-baseweb="select"] > div {
         min-height: 18px !important;
-        padding-top: 4px !important;
-        padding-bottom: 14px !important;
+        padding-top: 6px !important;
+        padding-bottom: 6px !important;
     }
             
 
