90ab6963edb04196f11e293896233f69c93c0158
fotopoulos-v <fotopoulos.v2@gmail.com>
2025-12-07 23:33:07 +0200

    changes vol69 to align the filters

--- BoardGame-Scout.py
+++ BoardGame-Scout.py
@@ -174,8 +174,8 @@ st.markdown("""
 
 /* Add spacing to the Mechanics element container within the expander */
 [data-testid="stExpander"] div.st-key-f_mechanics.stElementContainer {
-    margin-top: 0.1rem !important;
-    padding-top: 0.1rem !important;
+    margin-top: -0.1rem !important;
+    padding-top: -0.1rem !important;
 }       
 
 /* Add space after the Category multiselect, before the Mechanics label */
