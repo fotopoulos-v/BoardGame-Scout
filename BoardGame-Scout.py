126acda3ee7766250bb38be668361827792a98e9
fotopoulos-v <fotopoulos.v2@gmail.com>
2026-01-04 16:46:35 +0200

    fixing the recommended games button position v6

--- BoardGame-Scout.py
+++ BoardGame-Scout.py
@@ -1287,6 +1287,7 @@ st.markdown("""
         background-color: #4F1022 !important;   /* your favourite purple */
         color: white !important;
         width  : 190px !important;              /* make it wider */
+        margin-left: 24px !important;   /* push it to the right */
         height : 38px !important;
         margin-top: 0px !important;
         border: 1px solid #4F1022 !important;
@@ -1307,7 +1308,7 @@ st.markdown("""
 if st.session_state.get("show_user_section", False):
     st.subheader("🎮 Retrieve Your BGG Collection")
     
-    col_username, col_list, col_reveal, col_rec = st.columns([2, 1.5, 1.1, 2.5], gap='small')
+    col_username, col_list, col_reveal, col_rec = st.columns([1, 1.5, 1.4, 2], gap='small')
 
     with col_username:
         username = st.text_input("Enter your BoardGameGeek username:", label_visibility="collapsed", placeholder="BGG username")
