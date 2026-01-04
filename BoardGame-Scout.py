1d23659a02b1b38232f06c1fcea260169d4b60f1
fotopoulos-v <fotopoulos.v2@gmail.com>
2026-01-04 16:39:50 +0200

    fixing the recommended games button position v2

--- BoardGame-Scout.py
+++ BoardGame-Scout.py
@@ -1286,7 +1286,8 @@ st.markdown("""
     div.stElementContainer.st-key-rec_btn button:focus {
         background-color: #4F1022 !important;   /* your favourite purple */
         color: white !important;
-        width  : 160px !important;              /* make it wider */
+        margin-left: 24px !important;   /* push it to the right */
+        width  : 170px !important;              /* make it wider */
         height : 38px !important;
         margin-top: 0px !important;
         border: 1px solid #4F1022 !important;
