52016b65493de92df8060c0dd1bbe63c564960ad
fotopoulos-v <fotopoulos.v2@gmail.com>
2026-01-06 22:16:33 +0200

    fixing filter spacings v73

--- BoardGame-Scout.py
+++ BoardGame-Scout.py
@@ -272,12 +272,12 @@ st.markdown("""
 
     /* Match the height of number inputs */
     [data-testid="stExpander"] .stMultiSelect [data-baseweb="select"] {
-        min-height: 35px !important; 
-        max-height: 35px !important;  
+        min-height: 36px !important; 
+        max-height: 36px !important;  
     }
     
     [data-testid="stExpander"] .stMultiSelect [data-baseweb="select"] > div {
-        min-height: 35px !important;  
+        min-height: 36px !important;  
         padding-top: 4px !important; 
         padding-bottom: 4px !important; 
     }
