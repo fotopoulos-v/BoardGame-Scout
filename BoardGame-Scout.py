adc9cd60eb3e2063ecd05aa0bcaee0a7831c78bd
fotopoulos-v <fotopoulos.v2@gmail.com>
2026-05-10 01:13:19 +0300

    fixed promo links in the sidebar

--- BoardGame-Scout.py
+++ BoardGame-Scout.py
@@ -579,7 +579,7 @@ with st.sidebar:
     _app_dir = os.path.dirname(os.path.abspath(__file__))
     guru_logo_path = os.path.join(_app_dir, "assets", "images", "guru_logo.png")
     broke_logo_path = os.path.join(_app_dir, "assets", "images", "broke_logo.png")
-    boardgame_broke_url = "#"  # TODO: replace with your deployed BoardGame Broke URL
+    boardgame_broke_url = "https://boardgame-broke.streamlit.app/"
 
     def render_sidebar_promo_card(image_path, target_url, title, description, width=48):
         image_html = ""
